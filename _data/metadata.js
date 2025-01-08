83c170ce8ebf846ae68f87b23b8a3f327bf2abbe
Nitheesh Kumaar <38747369+nitheeshkumaarmv@users.noreply.github.com>
2025-01-08 13:30:19 +0530

    Add files via upload

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Nomad's Life",
+	url: "https://example.com/",
+	language: "en",
+	description: "Experiencing Life!",
+	author: {
+		name: "Your Name Here",
+		email: "youremailaddress@example.com",
+		url: "https://example.com/about-me/"
+	}
+}
